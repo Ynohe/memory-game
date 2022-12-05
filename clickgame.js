d91edcfe93b6bc84fbc0d9bedf2521a867b32fa2
monica-reverte <monicareverte24@gmail.com>
2022-12-05 11:26:08 +0100

    3 seconds javascript

---
+++ clickgame.js
@@ -0,0 +1,14 @@
+const seconds = document.getElementById("seconds");
+seconds.addEventListener("click", threeSeconds);
+const startFront = document.querySelectorAll(".front-image");
+const startBack = document.querySelectorAll(".back-image");
+
+function threeSeconds() {
+    startFront.forEach(front => {
+        front.style.animation = "start-front 3s";
+    });
+    
+    startBack.forEach(back => {
+        back.style.animation = "start-back 3s";
+    });
+}
